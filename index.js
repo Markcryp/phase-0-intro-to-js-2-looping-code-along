23b51cfe8ed335fae4ba5b540df936649a2cfefa
Mark <mark.njau@student.moringaschool.com>
2024-06-27 21:45:56 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const message = [];
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift`)
+    }
+    return message;
+}
+
+
+
+function countDown(num) {
+    for (let i = num; i>= 0; i--) {
+        console.log(i);
+    }
+}
